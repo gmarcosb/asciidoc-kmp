eb235f80c65d24e8f6af2b7bba7ae5469722ace0
Marcos Boyington <15697303+gmarcosb@users.noreply.github.com>
2026-05-19 15:56:17 -0600

    Cleanup

--- src/commonMain/kotlin/org/projectchip/alchemy/asciidoc/parse/AsciidocParseContext.kt
+++
@@ -1,8 +0,0 @@
-package org.projectchip.alchemy.asciidoc.parse
-
-import io.github.mirrgieriana.xarpeg.DefaultParseContext
-import org.projectchip.alchemy.asciidoc.Document
-
-class AsciidocParseContext(src: String, val document: Document = Document()) : DefaultParseContext(src) {
-    // Allows maintaining parser state, like tracking indent level or delimters
-}
